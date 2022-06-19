17ce62df2b96ea9a2d6035a4b8056f7fabb8d96e
prashantsingh2408 <prashantsingh2408@gmail.com>
2022-06-19 08:56:13 +0530

    add practice.rb

---
+++ practice.rb
@@ -0,0 +1,18 @@
+puts 'prashant'
+print 'hello'
+name = 'prashant'
+age = 24
+gpc = 3.2
+ismale = true
+flaws = nil 
+
+phrase = 'prashant singh'
+print phrase.index("A")
+
+
+num = 20.487
+puts num.round()
+puts num.ceil()
+puts num.floor()
+puts Math.sqrt(36)
+
