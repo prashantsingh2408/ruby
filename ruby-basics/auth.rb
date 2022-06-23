d3ca6f53b59a950a6f141589645dd983c53581e0
prashantsingh2408 <prashantsingh2408@gmail.com>
2022-06-23 14:05:40 +0530

    add ruby/ruby-basics/auth.rb

---
+++ ruby-basics/auth.rb
@@ -0,0 +1,38 @@
+# uses array where username and password are stored 
+users = [
+          {username: "admin", password: "password"},
+          {username: "user", password: "password"},
+          {username: "guest", password: "password"}
+        ]
+# authentication method to check and verify if username/password combination exists 
+def auth_user(username,password, list_of_users)
+  list_of_users.each do |user_record|
+    if user_record[:username] == username && user_record[:password] == password
+      return user_record
+    end
+   end
+   "Credentials were not correct"
+end
+
+# program execution flow
+puts "Welcome to the authenticator"
+25.times {print "-"}
+puts
+puts " This program will take input from the user and compare it to a list of users."
+puts " If the user is in the list, it will display their password"
+
+attempts = 1
+while attempts < 4
+  print "Username: "
+  username = gets.chomp
+  print "Password: "
+  password = gets.chomp
+  authentication = auth_user(username, password, users)
+  puts authentication
+  puts "Press n to quit or any other key to continue"
+  input = gets.chomp.downcase
+  break if input == "n"
+  attempts += 1
+end
+
+puts "You have exceeded the number of attempts" if attempts == 4
